7ad0e47a10752e8c290b691ed79f7da69f2c2e64
Raman Mehta <raman.mehta@fiftyfivetech.io>
2023-07-17 17:12:35 +0530

    parent project files

---
+++ src/main/java/com/fiftyfive/springboot/SpringbootKafkaProjrctApplication.java
@@ -0,0 +1,13 @@
+package com.fiftyfive.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaProjrctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaProjrctApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiftyfive/springboot/SpringbootKafkaProjrctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiftyfive.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaProjrctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
